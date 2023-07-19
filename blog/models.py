f8d5651924b638e63d90e18e8d52b323832496db
Carlos Jesus Caro <carlos.jesus.caro@gmail.com>
2023-07-19 11:13:58 -0400

    work in progress

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Blog(models.Model):
+    name = models.CharField(max_length=100)
+    synopsis = models.CharField(max_length=250)
+    date = models.DateField()
+    image = models.ImageField(upload_to="blog/images/")
+    url = models.URLField(blank=True)
+
+    def __str__(self):
+        return self.name
